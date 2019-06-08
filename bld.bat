df590a0ddf482c42c63ce0ec3e40c1959466059e
pvn <me@pedro-vicente.net>
2019-06-07 20:45:27 -0400

    build for Visual Studio 2019

--- bld.bat
+++ bld.bat
@@ -1,10 +1,11 @@
 
 @echo off
 if not defined DevEnvDir (
- call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
+ call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
  if errorlevel 1 goto :eof
 )
-
+set MSVC_VERSION="Visual Studio 16 2019"
+echo using %MSVC_VERSION%
 
 set root=%cd%
 :: replace the character string '\' with '/' needed for cmake
@@ -25,9 +26,11 @@ if exist %root%\external\openssl-1.1.0g\libssl.lib (
   popd
 )
 
-set MSVC_VERSION="Visual Studio 14 2015 Win64"
-cmake -H. -Bbuild -G %MSVC_VERSION% ^
+rm -rf CMakeCache.txt CMakeFiles
+cmake -H. -Bbuild -G "Visual Studio 16 2019" ^
+-A x64 ^
+-G %MSVC_VERSION% ^
 -DOPENSSL_INCLUDE:PATH=%root_cmake%/ssl/include ^
 -DOPENSSL_LIBRARY:FILE=%root_cmake%/ssl/lib/libssl.lib ^
 -DCRYPTO_LIBRARY:FILE=%root_cmake%/ssl/lib/libcrypto.lib
-msbuild %root%\build\lib_netsockets.sln /target:build /property:configuration=debug
+msbuild %root%\build\lib_netsockets.sln /target:build /property:configuration=debug /property:Platform=x64 /nologo /verbosity:minimal
