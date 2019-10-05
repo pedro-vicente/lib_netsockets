4c065aafc818020d951687f1d6df0a9d69eb86b3
pvn <me@pedro-vicente.net>
2019-10-05 00:39:21 -0400

    remove openSSL

--- bld.bat
+++ bld.bat
@@ -1,4 +1,3 @@
-
 @echo off
 if not defined DevEnvDir (
  call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
@@ -12,25 +11,8 @@ set root=%cd%
 set root_cmake=%root:\=/%
 echo cmake root is %root_cmake%
 
-:build_ssl
-if exist %root%\external\openssl-1.1.0g\libssl.lib (
- echo skipping openssl-1.1.0g build
-) else (
-  echo building openssl-1.1.0g
-  pushd external
-  pushd openssl-1.1.0g
-  perl Configure VC-WIN64A no-asm no-shared --debug --prefix=%root%\ssl 
-  nmake
-  nmake install
-  popd
-  popd
-)
-
 rm -rf CMakeCache.txt CMakeFiles
 cmake -H. -Bbuild -G "Visual Studio 16 2019" ^
 -A x64 ^
--G %MSVC_VERSION% ^
--DOPENSSL_INCLUDE:PATH=%root_cmake%/ssl/include ^
--DOPENSSL_LIBRARY:FILE=%root_cmake%/ssl/lib/libssl.lib ^
--DCRYPTO_LIBRARY:FILE=%root_cmake%/ssl/lib/libcrypto.lib
+-G %MSVC_VERSION% 
 msbuild %root%\build\lib_netsockets.sln /target:build /property:configuration=debug /property:Platform=x64 /nologo /verbosity:minimal
